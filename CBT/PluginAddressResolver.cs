32bc53384cd8262c6b752dbd096b3331bf10bcc3
Zeffuro <Jeffroiscool@gmail.com>
2025-06-05 00:16:25 +0200

    Update to API12 + Fix StatusCache
    Update to API12
    
    API12 Update + Fix StatusCache

--- CBT/PluginAddressResolver.cs
+++ CBT/PluginAddressResolver.cs
@@ -23,7 +23,7 @@ public class PluginAddressResolver : BaseAddressResolver
     {
         this.AddScreenLog = scanner.ScanText("E8 ?? ?? ?? ?? BF ?? ?? ?? ?? EB 39");
 
-        this.ReceiveActionEffect = scanner.ScanText("40 55 56 57 41 54 41 55 41 56 48 8D AC 24");
+        this.ReceiveActionEffect = scanner.ScanText("40 55 53 56 41 54 41 55 41 56 41 57 48 8D AC 24 ?? ?? ?? ?? 48 81 EC ?? ?? ?? ?? 48 8B 05 ?? ?? ?? ?? 48 33 C4 48 89 45 70");
 
         Service.PluginLog.Debug($"{nameof(this.AddScreenLog)}           0x{this.AddScreenLog:X}");
         Service.PluginLog.Debug($"{nameof(this.ReceiveActionEffect)}    0x{this.ReceiveActionEffect:X}");
